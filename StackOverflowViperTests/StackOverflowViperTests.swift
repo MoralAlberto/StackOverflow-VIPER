a59ec31d36d9017fae709295c4505eb5712464b7
Alberto Moral <alberto.moral@zyncro.com>
2015-12-17 14:17:35 +0100

    Initial Commit

---
+++ StackOverflowViperTests/StackOverflowViperTests.swift
@@ -0,0 +1,36 @@
+//
+//  StackOverflowViperTests.swift
+//  StackOverflowViperTests
+//
+//  Created by Moral on 17/12/15.
+//  Copyright © 2015 Alberto Moral. All rights reserved.
+//
+
+import XCTest
+@testable import StackOverflowViper
+
+class StackOverflowViperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
